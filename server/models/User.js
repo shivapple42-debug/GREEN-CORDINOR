8fb5ad073f4a1c6c0f67997ba1e2d6453a7be1d8
Shiv Patel <shivapple42@gmail.com>
2026-07-27 10:18:18 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Original Password (તમારા કહેવા મુજબ)
+  password: { type: String, required: true },
   department: { type: String, required: true },
   isApproved: { type: Boolean, default: false },
   status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
